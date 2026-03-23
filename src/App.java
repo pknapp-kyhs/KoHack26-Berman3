07593b21f284391d07f65bee7f980f4dd91dfa9e
Ezra Lederman <ezra.lederman@mjbha.org>
2026-03-23 18:02:58 +0000

    turned on CLI by default

--- src/App.java
+++ src/App.java
@@ -2,14 +2,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        // Uncomment to use GUI version
         // Substitutes substitutes = new Substitutes();
         // GUI.makeGUI(substitutes);
 
         // Uncomment to use CLI version 
-        // Substitutes substitutes = new Substitutes("Recipes.txt");
-        // Input.getAllPreferences(substitutes);
-        // System.out.println(substitutes.returnAllAllergens());
-        // System.out.println(substitutes.substituteAll());
+        Substitutes substitutes = new Substitutes("Recipes.txt");
+        Input.getAllPreferences(substitutes);
+        System.out.println(substitutes.returnAllAllergens());
+        System.out.println(substitutes.substituteAll());
 
     }
 }
